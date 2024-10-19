6a2cf2a323e71210ab8167c9bb2e8c2fe0e940be
自贡市第一中学校 <zigongyizhong@proton.me>
2024-10-19 23:45:42 +0800

    大量更新
    1. 文件名保存为视频标题
    2. 修复进度条问题
    3. 添加快速打开文件夹菜单
    4. 调用yt-dlp支持多平台

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import os
 import shutil
 import psutil
 from urllib.parse import urljoin
+import urllib
 import subprocess
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QTextEdit, QPushButton, QProgressBar, QComboBox, QFileDialog, QTabWidget, QCheckBox, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPlainTextEdit, QTextEdit, QPushButton, QProgressBar, QComboBox, QFileDialog, QTabWidget, QCheckBox, QDesktopWidget, QMenuBar, QAction, QMessageBox
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QThread, pyqtSignal, QProcess
 from datetime import datetime
+import yt_dlp
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -32,6 +34,7 @@ seal_path = resource_path("zgyz_seal.ico")
 class DownloadThread(QThread):
     progress_update = pyqtSignal(int, int, int, int)
     download_complete = pyqtSignal(str)
+    all_downloads_complete = pyqtSignal()
     error_occurred = pyqtSignal(str)
 
     def __init__(self, urls, cdnurl):
@@ -41,86 +44,170 @@ class DownloadThread(QThread):
 
     def run(self):
         for i, url in enumerate(self.urls):
-            guid, m3u8_url = self.get_webpage_extract_guid_and_generate_m3u8_url(url)
-            if guid and m3u8_url:
-                self.download_and_process_m3u8(guid, m3u8_url, i+1, len(self.urls))
+            if 'cctv.cn' in url or 'cctv.com' in url:
+                guid, m3u8_url, title = self.get_webpage_extract_guid_and_generate_m3u8_url(url)
+                if guid and m3u8_url:
+                    self.download_and_process_m3u8(guid, m3u8_url, i+1, len(self.urls), title)
+                else:
+                    self.error_occurred.emit(f"无法获取 GUID 或生成 m3u8 URL: {url}")
             else:
-                self.error_occurred.emit(f"无法获取 GUID 或生成 m3u8 URL: {url}")
+                self.download_with_ytdlp(url, i+1, len(self.urls))
+        
+        # 所有下载完成后发送信号
+        self.all_downloads_complete.emit()
+
+    def download_with_ytdlp(self, url, current_video, total_videos):
+        base_dir = os.path.join(os.getcwd(), datetime.now().strftime("%Y-%m-%d"))
+        os.makedirs(base_dir, exist_ok=True)
+
+        ydl_opts = {
+            'format': 'bestvideo+bestaudio/best',
+            'outtmpl': os.path.join(base_dir, '%(title)s.%(ext)s'),
+            'merge_output_format': 'mp4',
+            'progress_hooks': [lambda d: self.ytdlp_progress_hook(d, current_video, total_videos)],
+        }
+
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            self.download_complete.emit(f"Downloaded video from {url}")
+        except Exception as e:
+            self.error_occurred.emit(f"Error downloading {url}: {str(e)}")
+
+    def ytdlp_progress_hook(self, d, current_video, total_videos):
+        if d['status'] == 'downloading':
+            percent = d.get('_percent_str', '0%')
+            # 移除 ANSI 颜色代码
+            percent = re.sub(r'\x1b\[[0-9;]*m', '', percent)
+            percent = percent.replace('%', '').strip()
+            try:
+                progress = float(percent)
+                self.progress_update.emit(current_video, total_videos, int(progress), 100)
+            except ValueError:
+                # 如果无法转换为浮点数，就不更新进度
+                pass
+        elif d['status'] == 'finished':
+            self.progress_update.emit(current_video, total_videos, 100, 100)
 
     def get_webpage_extract_guid_and_generate_m3u8_url(self, url):
         try:
             response = requests.get(url, proxies={'http': None, 'https': None})
+            response.encoding = 'utf-8'  # 明确指定编码为 UTF-8
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
                 guid_pattern = r'var\s+guid(?:_0)?\s*=\s*"([^"]+)"'
                 video_center_id_pattern = r'videoCenterId:\s*"([^"]+)"'
+                title_pattern = r"var\s+(?:share|comment)Title\s*=\s*['\"]([^'\"]+)['\"];"
+                
                 script_tags = soup.find_all('script')
+                guid = None
+                video_center_id = None
+                title = None
+
                 for script in script_tags:
                     if script.string:
                         guid_match = re.search(guid_pattern, script.string)
                         if guid_match:
                             guid = guid_match.group(1)
-                            m3u8_url = f"https://{self.cdnurl}/asp//hls/2000/0303000a/3/default/{guid}/2000.m3u8"
-                            # m3u8_url = f"https://{self.cdnurl}/asp/hls/2000/0303000a/3/default/{guid}/2000.m3u8"
-                            return guid, m3u8_url
+                        
+                        title_match = re.search(title_pattern, script.string)
+                        if title_match:
+                            title = title_match.group(1)
 
-                video_center_id_match = re.search(video_center_id_pattern, response.text)
-                if video_center_id_match:
-                    video_center_id = video_center_id_match.group(1)
+                if not guid:
+                    video_center_id_match = re.search(video_center_id_pattern, response.text)
+                    if video_center_id_match:
+                        video_center_id = video_center_id_match.group(1)
+
+                if guid:
+                    m3u8_url = f"https://{self.cdnurl}/asp//hls/2000/0303000a/3/default/{guid}/2000.m3u8"
+                elif video_center_id:
                     m3u8_url = f"https://{self.cdnurl}/asp//hls/2000/0303000a/3/default/{video_center_id}/2000.m3u8"
-                    return video_center_id, m3u8_url
+                else:
+                    return None, None, None
 
-            return None, None
+                return guid or video_center_id, m3u8_url, title
+
+            return None, None, None
         except Exception as e:
             print(f"Error: {e}")
-            return None, None
+            return None, None, None
 
-    def download_and_process_m3u8(self, guid, m3u8_url, current_video, total_videos):
+    def download_and_process_m3u8(self, guid, m3u8_url, current_video, total_videos, title):
         base_dir = os.path.join(os.getcwd(), datetime.now().strftime("%Y-%m-%d"))
         os.makedirs(base_dir, exist_ok=True)
-        output_file = os.path.join(base_dir, f"{guid}.mp4")
+        
+        # 使用 GUID 作为临时文件名
+        temp_filename = f"{guid}.mp4"
+        temp_output_file = os.path.join(base_dir, temp_filename)
 
-        ffmpeg_command = [ffmpeg_path, "-i", m3u8_url, "-c", "copy", output_file,"-y"]
+        ffmpeg_command = [
+            ffmpeg_path,
+            "-i", m3u8_url,
+            "-c", "copy",
+            temp_output_file,
+            "-y"
+        ]
 
         try:
-            # 将 stderr 输出重定向到 PIPE
             process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, creationflags=subprocess.CREATE_NO_WINDOW)
 
             duration_regex = re.compile(r"Duration: (\d{2}):(\d{2}):(\d{2}\.\d{2})")
             time_regex = re.compile(r"time=(\d{2}):(\d{2}):(\d{2}\.\d{2})")
             duration = None
 
-            stdout, stderr = process.communicate()
-            
-            # 收集完整的输出
-            full_output = stdout + stderr
+            while True:
+                output = process.stderr.readline()
+                if output == '' and process.poll() is not None:
+                    break
+                if output:
+                    if not duration:
+                        match = duration_regex.search(output)
+                        if match:
+                            duration = self.time_to_seconds(match.groups())
 
-            if not duration:
-                match = duration_regex.search(full_output)
-                if match:
-                    duration = self.time_to_seconds(match.groups())
-
-            for line in full_output.splitlines():
-                match = time_regex.search(line)
-                if match and duration:
-                    current_time = self.time_to_seconds(match.groups())
-                    progress = int((current_time / duration) * 100)
-                    self.progress_update.emit(current_video, total_videos, progress, 100)
+                    match = time_regex.search(output)
+                    if match and duration:
+                        current_time = self.time_to_seconds(match.groups())
+                        progress = int((current_time / duration) * 100)
+                        self.progress_update.emit(current_video, total_videos, progress, 100)
 
             if process.returncode == 0:
+                # 下载成功，尝试重命名文件
+                # 下载成功后再命名是为了不让FFmpeg出错
+                if title:
+                    # 解码 URL 编码的字符串并移除非法字符
+                    safe_title = re.sub(r'[\\/*?:"<>|]', "", urllib.parse.unquote(title))
+                    final_filename = f"{safe_title}.mp4"
+                    final_output_file = os.path.join(base_dir, final_filename)
+                    try:
+                        os.rename(temp_output_file, final_output_file)
+                        output_file = final_output_file
+                    except Exception as rename_error:
+                        print(f"重命名文件时出错: {rename_error}")
+                        output_file = temp_output_file
+                else:
+                    output_file = temp_output_file
+
+                # 确保最后一个视频下载完成时显示100%进度
+                self.progress_update.emit(current_video, total_videos, 100, 100)
                 self.download_complete.emit(output_file)
             else:
                 error_message = f"下载失败: FFmpeg 进程返回错误码 {process.returncode}\n"
                 error_message += f"命令: {' '.join(ffmpeg_command)}\n"
-                error_message += f"输出:\n{full_output}"
                 self.error_occurred.emit(error_message)
 
         except Exception as e:
             error_message = f"下载过程中发生错误: {str(e)}\n"
             error_message += f"命令: {' '.join(ffmpeg_command)}\n"
-            error_message += f"异常详情: {full_output}"
             self.error_occurred.emit(error_message)
 
+        finally:
+            # 确保进程被正确关闭
+            if process.poll() is None:
+                process.terminate()
+                process.wait()
+                
     def time_to_seconds(self, time_tuple):
         hours, minutes, seconds = map(float, time_tuple)
         return hours * 3600 + minutes * 60 + seconds
@@ -343,6 +430,19 @@ class DownloaderGUI(QWidget):
     def initUI(self):
         layout = QVBoxLayout()
 
+        # 添加菜单栏
+        menubar = QMenuBar()
+        layout.setMenuBar(menubar)
+
+        openDirAction = QAction('打开视频保存目录', self)
+        openDirAction.triggered.connect(self.openProgramDirectory)
+        menubar.addAction(openDirAction)
+
+        aboutAction = QAction('关于', self)
+        aboutAction.triggered.connect(self.showAbout)
+        menubar.addAction(aboutAction)
+
+
         # Add FFmpeg acceleration dropdown
         acceleration_layout = QHBoxLayout()
         acceleration_layout.addWidget(QLabel('FFmpeg加速:'))
@@ -359,7 +459,7 @@ class DownloaderGUI(QWidget):
         layout.addWidget(self.tab_widget)
 
         self.setLayout(layout)
-        self.setWindowTitle('自贡一中放送系统')
+        self.setWindowTitle('自贡一中新闻采集系统')
         self.setWindowIcon(QIcon(seal_path))
         
         screen = QDesktopWidget().screenNumber(self)
@@ -372,19 +472,38 @@ class DownloaderGUI(QWidget):
         
         self.setGeometry(x, y, width, height)
 
+    def openProgramDirectory(self):
+        program_dir = os.path.dirname(os.path.abspath(__file__))
+        if sys.platform == 'win32':
+            os.startfile(program_dir)
+        elif sys.platform == 'darwin':
+            subprocess.Popen(['open', program_dir])
+        else:
+            subprocess.Popen(['xdg-open', program_dir])
+
+    def showAbout(self):
+        about_text = """本程序用于自贡市第一中学校校园电视台新闻采集
+自贡一中计算机社制作
+
+===培德修身，博学增能===
+
+Github: https://github.com/zigongyizhong/broadcast
+自贡一中: http://www.zgyz.net"""
+        QMessageBox.about(self, "关于", about_text)
+
     def create_download_tab(self):
         download_widget = QWidget()
         layout = QVBoxLayout()
 
         url_layout = QHBoxLayout()
         url_layout.addWidget(QLabel('URLs:'))
-        self.url_input = QTextEdit()
-        self.url_input.setPlaceholderText("每行输入一个URL")
+        self.url_input = QPlainTextEdit()  # 使用 QPlainTextEdit 替代 QTextEdit
+        self.url_input.setPlaceholderText("每行输入一个URL\n支持央视超清、哔哩哔哩、Youtube、ニコニコ等等")
         url_layout.addWidget(self.url_input)
         layout.addLayout(url_layout)
 
         cdn_layout = QHBoxLayout()
-        cdn_layout.addWidget(QLabel('CDN:'))
+        cdn_layout.addWidget(QLabel('央视CDN:'))
         self.cdn_combo = QComboBox()
         self.cdn_combo.addItems(list(self.cdn_urls.keys()))
         cdn_layout.addWidget(self.cdn_combo)
@@ -553,24 +672,31 @@ class DownloaderGUI(QWidget):
         self.download_thread = DownloadThread(urls, cdnurl)
         self.download_thread.progress_update.connect(self.update_progress)
         self.download_thread.download_complete.connect(self.download_finished)
+        self.download_thread.all_downloads_complete.connect(self.all_downloads_finished)
         self.download_thread.error_occurred.connect(self.show_error)
         self.download_thread.start()
 
         self.download_btn.setEnabled(False)
         self.status_text.setText("下载中...")
+        self.progress_bar.setValue(0)  # 重置进度条
 
     def update_progress(self, current_video, total_videos, current_segment, total_segments):
-        progress = int((current_video - 1) / total_videos * 100+ (current_segment / total_segments) * (100 /total_videos))
+        progress = int((current_video - 1) / total_videos * 100 + (current_segment / total_segments) * (100 / total_videos))
         self.progress_bar.setValue(progress)
         self.status_text.setText(f"下载视频 {current_video}/{total_videos}，当前视频进度 {current_segment}/{total_segments}")
 
     def download_finished(self, output_file):
         self.status_text.append(f"下载完成: {output_file}")
+
+    def all_downloads_finished(self):
+        self.progress_bar.setValue(100)  # 确保进度条显示100%
+        self.status_text.append("所有视频下载完成")
         self.download_btn.setEnabled(True)
 
     def show_error(self, error_message):
         self.status_text.append(error_message)
         self.download_btn.setEnabled(True)
+
 
     def start_conversion(self):
         input_file = self.file_input.toPlainText()
